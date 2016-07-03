9ca77d1724974d40dffdb3a438661e0326ebab78
Claudio Greco <claudiogaetanogreco@gmail.com>
2016-07-03 12:39:36 +0200

    Added code documentation; Improved code style.

--- scraper.py
+++ scraper.py
@@ -1,23 +1,27 @@
-import time
-import validators
 import socket
+import time
 from collections import deque
 from enum import Enum
 from itertools import islice
 
+import validators
 from github import Github
+from github.GithubException import GithubException
 from github.NamedUser import NamedUser
 from github.Organization import Organization
 from github.Repository import Repository
-from github.GithubException import GithubException
 from rdflib import Namespace, Graph, RDF, Literal, URIRef
 from rdflib.namespace import XSD
 
 
 class SeedType(Enum):
+    """
+    Class which represents the type of the seed node.
+    """
     user = 1
     repo = 2
     org = 3
+
 
 github2owl_users = Namespace("http://github2owl.org/users/")
 github2owl_repos = Namespace("http://github2owl.org/repos/")
@@ -25,7 +29,24 @@ github2owl_orgs = Namespace("http://github2owl.org/orgs/")
 schema = Namespace("http://schema.org/")
 
 
+def sanitize(text):
+    """
+    Removes spaces from the given text.
+
+    :param text: Text to be fixed
+    :return: Fixed text
+    """
+    return text.replace(" ", "")
+
+
 def describe_user_node(graph, node, node_iri):
+    """
+    Generates the RDF representation of the given user node and adds it to the graph.
+
+    :param graph: RDF graph
+    :param node: User node to be described
+    :param node_iri: Identifier associated to the given node
+    """
     graph.add((node_iri, RDF.type, schema.Person))
     graph.add((node_iri, schema.alternateName, Literal(node.login, datatype=XSD.string)))
     if node.name:
@@ -45,6 +66,13 @@ def describe_user_node(graph, node, node_iri):
 
 
 def describe_org_node(graph, node, node_iri):
+    """
+    Generates the RDF representation of the given organization node and adds it to the graph.
+
+    :param graph: RDF graph
+    :param node: Organization node to be described
+    :param node_iri: Identifier associated to the given node
+    """
     graph.add((node_iri, RDF.type, schema.Organization))
     graph.add((node_iri, schema.alternateName, Literal(node.login, datatype=XSD.string)))
     if node.name:
@@ -63,11 +91,14 @@ def describe_org_node(graph, node, node_iri):
             graph.add((node_iri, schema.url, URIRef(sanitized_url)))
 
 
-def sanitize(text):
-    return text.replace(" ", "")
-
-
 def describe_repo_node(graph, node, node_iri):
+    """
+    Generates the RDF representation of the given repository node and adds it to the graph.
+
+    :param graph: RDF graph
+    :param node: Repository node to be described
+    :param node_iri: Identifier associated to the given node
+    """
     graph.add((node_iri, RDF.type, schema.SoftwareSourceCode))
     if node.html_url:
         graph.add((node_iri, schema.codeRepository, URIRef(node.html_url)))
@@ -84,6 +115,14 @@ def describe_repo_node(graph, node, node_iri):
 
 
 def get_seed_node(github, seed_name, seed_type):
+    """
+    Returns the node associated to the given seed name and seed type.
+
+    :param github: Client of the GitHub API
+    :param seed_name: GitHub nickname to be used as seed
+    :param seed_type: Type of the node to be used as seed
+    :return: Node associated to the given seed name and seed type
+    """
     if seed_type == SeedType.user:
         return github.get_user(seed_name)
     if seed_type == SeedType.repo:
@@ -93,6 +132,16 @@ def get_seed_node(github, seed_name, seed_type):
 
 
 def pause_requests(github, github_username, github_password, margin=500, sleep_time=350):
+    """
+    Pause requests to respect GitHub API limits.
+    Recreates a GitHub client when limits have been reset.
+
+    :param github: Client of the GitHub API
+    :param github_username: Username of the GitHub API account
+    :param github_password: Password of the GitHub API account
+    :param margin: Minimum number of the remaining requests
+    :param sleep_time: Number of seconds to wait
+    """
     remaining_requests = github.rate_limiting[0]
     print("-- Remaining requests {0}".format(remaining_requests))
     while github.rate_limiting[0] <= margin:
@@ -112,6 +161,22 @@ def build_graph(github,
                 max_repos=30,
                 max_orgs=30,
                 max_iterations=5):
+    """
+    Builds the RDF graph scraped using the GitHub API.
+
+    :param github: Client of the GitHub API
+    :param github_username: Username of the GitHub API account
+    :param github_password: Password of the GitHub API account
+    :param seed_name: GitHub nickname to be used as seed
+    :param seed_type: Type of the node to be used as seed
+    :param max_following: Maximum number of following relations
+    :param max_contributors: Maximum number of contributors
+    :param max_members: Maximum number of organization members
+    :param max_repos: Maximum number of repositories
+    :param max_orgs: Maximum number of organizations
+    :param max_iterations: Maximum number of iterations for the scraping procedure
+    :return: RDF graph of the dataset
+    """
     nodes_queue = deque([get_seed_node(github, seed_name, seed_type)])
     visited_nodes = set()
     nodes_iris = {}
@@ -127,6 +192,7 @@ def build_graph(github,
         node = nodes_queue.popleft()
         try:
             pause_requests(github, github_username, github_password)
+
             if isinstance(node, NamedUser):
                 print("-- Username: {0}, iteration {1} of {2}"
                       .format(node.login, num_iterations, max_iterations))
@@ -148,13 +214,13 @@ def build_graph(github,
                 for repo in islice(node.get_repos(), 0, max_repos):
                     repo_name = repo.full_name.replace("/", "-")
                     if repo_name not in visited_nodes:
-                        nodes_queue.append(repo)                    
+                        nodes_queue.append(repo)
                     if repo_name not in nodes_iris:
                         nodes_iris[repo_name] = github2owl_repos[repo_name]
                     graph.add((github2owl_users[node.login], schema.creator, nodes_iris[repo_name]))
-                
+
                 pause_requests(github, github_username, github_password)
-                
+
                 for orgs in islice(node.get_orgs(), 0, max_orgs):
                     org_name = orgs.login
                     if org_name not in visited_nodes:
@@ -163,9 +229,10 @@ def build_graph(github,
                         nodes_iris[org_name] = github2owl_orgs[org_name]
                     graph.add((github2owl_users[node.login], schema.memberOf, nodes_iris[org_name]))
                 visited_nodes.add(node.login)
+
             elif isinstance(node, Organization):
                 org_name = node.login
-                
+
                 print("-- Organization: {0}, iteration {1} of {2}"
                       .format(org_name, num_iterations, max_iterations))
 
@@ -191,6 +258,7 @@ def build_graph(github,
                         nodes_iris[repo_name] = github2owl_repos[repo_name]
                     graph.add((github2owl_orgs[org_name], schema.creator, nodes_iris[repo_name]))
                 visited_nodes.add(node.login)
+
             elif isinstance(node, Repository):
                 print("-- Repository: {0}, iteration {1} of {2}"
                       .format(node.full_name, num_iterations, max_iterations))
@@ -209,9 +277,9 @@ def build_graph(github,
                         else:
                             nodes_iris[contributor.login] = github2owl_orgs[contributor.login]
                     graph.add((github2owl_repos[repo_name], schema.contributor, nodes_iris[contributor.login]))
-                
+
                 pause_requests(github, github_username, github_password)
-                
+
                 repo_creator = node.owner
                 if repo_creator.login not in visited_nodes:
                     nodes_queue.append(repo_creator)
@@ -221,10 +289,11 @@ def build_graph(github,
                     else:
                         nodes_iris[contributor.login] = github2owl_orgs[contributor.login]
                 graph.add((github2owl_repos[repo_name], schema.creator, nodes_iris[repo_creator.login]))
-                
+
                 visited_nodes.add(repo_name)
 
             num_iterations += 1
+
         except GithubException:
             print("Skipped blocked repository.")
         except (socket.error, socket.gaierror):
@@ -258,17 +327,17 @@ def build_graph(github,
                     repo_name = repo.full_name.replace("/", "-")
                     if repo_name in nodes_iris:
                         graph.add((nodes_iris[node.login], schema.creator, nodes_iris[repo_name]))
-                
+
                 pause_requests(github, github_username, github_password)
-                
+
                 for org in islice(node.get_orgs(), 0, max_orgs):
                     org_name = org.login
                     if org_name in nodes_iris:
                         graph.add((nodes_iris[node.login], schema.memberOf, nodes_iris[org_name]))
-                        
+
             elif isinstance(node, Organization):
                 org_name = node.login
-                
+
                 print("-- Organization: {0}, iteration {1} of {2}"
                       .format(org_name, num_iterations, max_iterations))
 
@@ -301,7 +370,7 @@ def build_graph(github,
                     if contributor.login in nodes_iris:
                         graph.add(
                             (nodes_iris[contributor.login], schema.contributor, nodes_iris[repo_name]))
-                
+
             queue_elements += 1
 
         except GithubException:
@@ -309,6 +378,4 @@ def build_graph(github,
         except (socket.error, socket.gaierror):
             print("Connection error! Reconnecting...")
             github = Github(github_username, github_password)
-
     return graph
-
